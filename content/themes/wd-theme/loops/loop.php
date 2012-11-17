c9b49b788626c4206349ed79bb11581335d55648
root <root@fozzy.anthonyticknor.com>
2012-11-17 13:51:47 -0600

    setting up base grid split and sideber in the loop

--- content/themes/wd-theme/loops/loop.php
+++ content/themes/wd-theme/loops/loop.php
@@ -1,20 +1,34 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-    <div class="feature">
-        <?php if (the_first_image() != '') { ?>
-        <div class="feature-img">
-            <img src="<?php echo the_first_image() ?>" alt="" />
+        <div class="content">
+            <div class="gridRow">
+                <div class="gridRow-col gridRow-col_size3of4" role="main">
+                    <div class="panel">
+                        
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <div class="feature">
+                            <?php if (the_first_image() != '') { ?>
+                            <div class="feature-img">
+                                <img src="<?php echo the_first_image() ?>" alt="" />
+                            </div>
+                            <?php } ?>
+                            <div class="feature-hd">
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            </div>
+                            <div class="feature-meta">
+                                <div class="date"><?php the_date(); ?></div>
+                            </div>
+                            <div class="feature-bd">
+                                <div class="user-content"><?php the_excerpt(); ?></div>
+                            </div>
+                        </div>
+                    <?php endwhile; endif; ?>
+                        
+                    </div>
+                </div>
+                <div class="gridRow-col gridRow-col_size1of4" role="complementary">
+                    <div class="panel">
+                        <?php get_sidebar(); ?>
+                    </div>
+                </div>
+            </div>
+        
         </div>
-        <?php } ?>
-        <div class="feature-hd">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        </div>
-        <div class="feature-meta">
-            <div class="date"><?php the_date(); ?></div>
-        </div>
-        <div class="feature-bd">
-            <div class="user-content"><?php the_excerpt(); ?></div>
-        </div>
-    </div><!-- #post-<?php the_ID(); ?> -->
-    
-<?php endwhile; endif; ?>
