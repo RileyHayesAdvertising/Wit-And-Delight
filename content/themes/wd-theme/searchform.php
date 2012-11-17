d710a45a32051f46856bd0627ec39b316b87785f
root <root@fozzy.anthonyticknor.com>
2012-11-17 14:25:56 -0600

    hooking up and testing search results

--- content/themes/wd-theme/searchform.php
+++ content/themes/wd-theme/searchform.php
@@ -1,4 +1,4 @@
-<form method="get" action="<?php echo home_url( '/' ); ?>" role="search" class="quickSearch"> <!-- TODO Wire up legitamate search -->
+<form method="get" action="<?php echo home_url( '/' ); ?>" role="search" class="quickSearch">
     <fieldset>
         <label for="s" class="isHidden">Search</label>
         <input type="search" class="input" id="s" name="s" placeholder="SEARCH" />
