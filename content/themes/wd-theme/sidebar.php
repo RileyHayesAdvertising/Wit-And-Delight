9918a1c1dd75ba72d47cdcf7e06f4dd091ccbf1a
root <root@fozzy.anthonyticknor.com>
2012-11-17 22:25:14 -0600

    adjusting tel link

--- content/themes/wd-theme/sidebar.php
+++ content/themes/wd-theme/sidebar.php
@@ -28,7 +28,7 @@
     <h2 class="isHidden">Contact Info</h2>
     <ul>
         <li><a href="mailto:witanddelight@gmail.com">witanddelight@gmail.com</a></li>
-        <li><a href="tel:612-845-6858">612 845 6858</a></li>
+        <li><a href="tel:6128456858">612 845 6858</a></li>
         <li><a href="#">Linked In</a></li> <!-- TODO Add Link -->
         <li><a href="#">Behance</a></li> <!-- TODO Add Link -->
         <li><a href="#">Dribbble</a></li> <!-- TODO Add Link -->
