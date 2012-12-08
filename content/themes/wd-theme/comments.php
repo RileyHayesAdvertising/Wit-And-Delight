f181845e88b87544477fad9d19e55141edf0435d
root <root@fozzy.anthonyticknor.com>
2012-12-07 20:12:58 -0600

    base comment markup output

--- content/themes/wd-theme/comments.php
+++ content/themes/wd-theme/comments.php
@@ -9,10 +9,15 @@
  * @version $Id$
  */
 ?>
-            <div id="comments">
+
+
+
 <?php if ( post_password_required() ) : ?>
-                <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyten' ); ?></p>
-            </div><!-- #comments -->
+<div id="panel panel_bare">
+    <div class="user-content">
+        <p><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyten' ); ?></p>
+    </div>
+</div><!-- // end comments panel -->
 <?php
         /* Stop the rest of comments.php from being processed,
          * but don't kill the script entirely -- we still have
@@ -22,48 +27,25 @@
     endif;
 ?>
 
-<?php
-    // You can start editing here -- including this comment!
-?>
+<div class="panel panel_bare">
 
 <?php if ( have_comments() ) : ?>
-            <h3 id="comments-title"><?php
-            printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'twentyten' ),
-            number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
-            ?></h3>
-
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-            <div class="navigation">
-                <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-            </div> <!-- .navigation -->
-<?php endif; // check for comment navigation ?>
-
-            <ol class="commentlist">
+    <div class="feature">
+        <div class="feature-hd">
+            <h1 class="hdg hdg_1">Comments (<?php comments_number('0','1','%'); ?>)</h3>
+        </div>
+        <div class="feature-bd">
+            <ol>
                 <?php
                     wp_list_comments();
-// Customize comment output: http://codex.wordpress.org/Function_Reference/wp_list_comments
+                    // Customize comment output: http://codex.wordpress.org/Function_Reference/wp_list_comments
                 ?>
             </ol>
-
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-            <div class="navigation">
-                <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-            </div><!-- .navigation -->
-<?php endif; // check for comment navigation ?>
-
-<?php else : // or, if we don't have comments:
-
-    /* If there are no comments and comments are closed,
-     */
-    if ( ! comments_open() ) :
-?>
-    <p class="nocomments">Comments are closed</p>
-<?php endif; // end ! comments_open() ?>
+        </div>
+    </div>
 
 <?php endif; // end have_comments() ?>
 
 <?php comment_form(); ?>
 
-</div><!-- #comments -->
+</div><!-- // end comments panel -->
