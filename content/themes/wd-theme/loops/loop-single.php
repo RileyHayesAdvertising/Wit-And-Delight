a468e0325bc3a43ef7cdfa341247b7cd95b6027c
karends <karends@ps147941.dreamhostps.com>
2013-01-01 13:37:14 -0800

    adding box to single post page content

--- content/themes/wd-theme/loops/loop-single.php
+++ content/themes/wd-theme/loops/loop-single.php
@@ -4,23 +4,25 @@
                     <div class="panel panel_condensed">
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                         <div class="panel panel_inverse">
-                            <div class="feature feature_condensed">
-                                <div class="feature-hd">
-                                    <h2 class="hdg hdg_1"><?php the_date('M.d.Y','',' | '); ?><?php the_title(); ?></h2>
-                                </div>
-                                <div class="feature-bd">
-                                    <div class="user-content">
-                                        <?php
-                                            the_content(); 
-                                        ?>
+                            <div class="box">
+                                <div class="feature feature_condensed">
+                                    <div class="feature-hd">
+                                        <h2 class="hdg hdg_1"><?php the_date('M.d.Y','',' | '); ?><?php the_title(); ?></h2>
                                     </div>
-                                </div>
-                                <div class="feature-meta">
-                                    <ul class="blocks blocks_3up">
-                                        <li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" class="btn" rel="external">Share on Facebook</a></li>
-                                        <li><a href="http://twitter.com/share?url=<?php the_permalink(); ?>" class="btn" rel="external">Tweet It</a></li>
-                                        <li><a href="http://pinterest.com/pin/create/button/?media=<?php echo urlencode(the_first_image()); ?>&url=<?php echo urlencode(get_permalink()); ?>" class="btn">Pin it</a></li>
-                                    </ul>
+                                    <div class="feature-bd">
+                                        <div class="user-content">
+                                            <?php
+                                                the_content(); 
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="feature-meta">
+                                        <ul class="blocks blocks_3up">
+                                            <li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" class="btn" rel="external">Share on Facebook</a></li>
+                                            <li><a href="http://twitter.com/share?url=<?php the_permalink(); ?>" class="btn" rel="external">Tweet It</a></li>
+                                            <li><a href="http://pinterest.com/pin/create/button/?media=<?php echo urlencode(the_first_image()); ?>&url=<?php echo urlencode(get_permalink()); ?>" class="btn">Pin it</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                             <div class="panel panel_bare panel_clip">
