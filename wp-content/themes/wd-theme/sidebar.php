5cf616d403e68d73e9bca8749a03f2c6d3ad0275
Anthony Ticknor <tony.ticknor@gmail.com>
2014-12-20 17:20:22 -0600

    updating sidebar with new instagram feed and popular posts while removing other part of the sidebar that are deprecated

--- wp-content/themes/wd-theme/sidebar.php
+++ wp-content/themes/wd-theme/sidebar.php
@@ -1,116 +1,28 @@
-<div class="panel isLargeView">
+<div class="pane">
     <div class="feature feature_condensed">
-        <div class="feature-hd feature-hd_alt">
+        <div class="feature-hd feature-hd_alt feature-hd_isCentered">
             <h2 class="hdg hdg_4">
-                <span class="hdg-split">Discovering the</span>
-                <span class="hdg-split">Wit &amp; Delight in</span>
+                W&D on Instagram
             </h2>
         </div>
         <div class="feature-bd feature-bd_condensed">
-            <ul class="vList vList_push vList_navFeatured">
-                <li><a href="/?cat=14">Fashion</a></li>
-                <li><a href="/?cat=18">Design</a></li>
-                <li><a href="/?cat=76">Products</a></li>
-                <li><a href="/?cat=5">&amp; Oddities</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="panel isLargeView">
-    <div class="feature js-toggle">
-        <div class="feature-hd js-toggle-link" data-cookie="js-toggle-categories">
-            <h2 class="hdg hdg_4">See Categories</h2>
-        </div>
-        <div class="feature-bd feature-bd_condensed js-toggle-target">
-            <ul class="vList vList_push vList_nav">
-                <?php wp_list_categories('orderby=count&order=desc&style=list&title_li='); ?>
-            </ul>
-        </div>
-    </div>
-    <div class="feature feature_condensed js-toggle">
-        <div class="feature-hd js-toggle-link" data-cookie="js-toggle-tags">
-            <h2 class="hdg hdg_4">See Tags</h2>
-        </div>
-        <div class="feature-bd feature-bd_condensed js-toggle-target">
-            <ul class="vList vList_push vList_nav">
-                <?php tag_list(); ?>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="panel isLargeView">
-    <div class="feature feature_condensed js-toggle">
-        <div class="feature-hd js-toggle-link" data-cookie="js-toggle-daily">
-            <h2 class="hdg hdg_4">Daily Reads</h2>
-        </div>
-        <div class="feature-bd feature-bd_condensed js-toggle-target">
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push isHidden">
-                    <h3 class="hdg hdg_5">Featured</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=3&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
-            </div>
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push">
-                    <h3 class="hdg hdg_5">Lifestyle</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=124&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
-            </div>
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push">
-                    <h3 class="hdg hdg_5">Stylish People</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=125&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
-            </div>
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push">
-                    <h3 class="hdg hdg_5">Kitchen Inspiration</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=127&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
-            </div>
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push">
-                    <h3 class="hdg hdg_5">Tumblr</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=106&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
-            </div>
-            <div class="feature feature_reasonable">
-                <div class="feature-hd feature-hd_alt feature-hd_push">
-                    <h3 class="hdg hdg_5">Friends</h3>
-                </div>
-                <div class="feature-bd feature-bd_condensed">
-                    <ul class="vList vList_push vList_nav">
-                        <?php wp_list_bookmarks('categorize=0&category=128&category_orderby=slug&title_li=0&show_private=0&orderby=name&show_images=0&show_description=0&show_name=1&show_rating=0&show_updated=0'); ?>
-                    </ul>
-                </div>
+            <div class="instagramFeedSidebar">
+                <?php /* configure in control panel */ echo do_shortcode( '[instagram-feed]' ); ?>
             </div>
         </div>
     </div>
 </div>
-<div class="panel">
-    <h2 class="isHidden">Contact Info</h2>
-    <ul class="vList vList_navFeatured">
-        <li><a href="mailto:<?php the_field('email_address', 'options'); ?>"><?php the_field('email_address', 'options'); ?></a></li>
-        <li><a href="<?php the_field('behance_link', 'options'); ?>" rel="external">Behance</a></li>
-    </ul>
+<div class="pane pane_inset">
+    <div class="feature feature_condensed">
+        <div class="feature-hd feature-hd_alt feature-hd_isCentered">
+            <h2 class="hdg hdg_4">
+                Popular Posts
+            </h2>
+        </div>
+        <div class="feature-bd feature-bd_condensed">
+            <div class="">
+            <?php wpp_get_mostpopular( 'limit=3&range="monthly"&order_by="comments"&post_type="post"&stats_views=0&wpp_start="<ol class=\'popularPosts\'>"&wpp_end="</ol">&post_html="<li><a href=\'{url}\'>{text_title}</a></li>"' ); ?>
+            </div>
+        </div>
+    </div>
 </div>
