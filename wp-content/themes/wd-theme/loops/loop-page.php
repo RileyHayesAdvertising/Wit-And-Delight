d8fb3d43165012522d65603ee7b5aed96b070fe1
Anthony Ticknor <tony.ticknor@gmail.com>
2015-01-03 13:21:27 -0600

    combining press and about pages

--- wp-content/themes/wd-theme/loops/loop-page.php
+++ wp-content/themes/wd-theme/loops/loop-page.php
@@ -17,6 +17,28 @@
                             </div>
                         </div>
                     <?php endwhile; endif; ?>
+                    <?php if(get_field('press')): ?>
+                        <div class="ancillary">
+                            <div class="ancillary-hd">
+                                <h2 class="hdg hdg_2">Press</h2>
+                            </div>
+                            <div class="ancillary-bd">
+                                <ul class="pods">
+                                    <?php while(has_sub_field('press')): ?>
+                                    <li>
+                                        <?php if(get_sub_field('press_link')) { ?><a href="<?php the_sub_field('press_link'); ?>"><?php } ?>
+                                        <div class="box box_tiny">
+                                            <?php the_sub_field('press_line_1'); ?><br/>
+                                            <span class="hdg mix-hdg_snowflake mix-hdg_snowflake_tight"><?php the_sub_field('press_line_2'); ?></span><br/>
+                                            <?php the_sub_field('press_line_3'); ?>
+                                        </div>
+                                        <?php if(get_sub_field('press_link')) { ?></a><?php } ?>
+                                    </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            </div>
+                        </div>
+                    <?php endif; ?>
                 </div>
                 <div class="gridRow-col gridRow-col_size1of4" role="complementary">
                     <?php get_sidebar(); ?>
