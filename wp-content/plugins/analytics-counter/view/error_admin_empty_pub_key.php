bb4ec3a52321c1ffde8e35b5fd55b3328f3578e2
Anthony Ticknor <tticknor@rileyhayes.com>
2016-03-31 17:16:21 -0500

    removing analytics counter in favor of different plugin

--- wp-content/plugins/analytics-counter/view/error_admin_empty_pub_key.php
+++
@@ -1,3 +0,0 @@
-<?php
-$url = admin_url('options-general.php?page=wpadm-ga-menu-settings');
-echo '<div class="error"><p>The site reports about error! Please deactivate and activate plugin '. WPADM_GA__PLUGIN_NAME . '</p></div>';
--- wp-content/plugins/analytics-counter/view/error_admin_wpadm_error.php
+++
@@ -1,5 +0,0 @@
-<?php
-$error = $_GET['error'];
-$error = str_replace('_', ' ', $error);
-
-echo '<div class="error"><p>The site reports "'.$error.'"</p></div>';
