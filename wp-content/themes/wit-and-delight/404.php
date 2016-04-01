a52caf048e93fa89abb33fac091037e2daa9b41d
Anthony Ticknor <tticknor@rileyhayes.com>
2016-03-31 22:07:55 -0500

    404 page styles

--- wp-content/themes/wit-and-delight/404.php
+++ wp-content/themes/wit-and-delight/404.php
@@ -4,15 +4,16 @@
     <div class="wrapper">
         <div class="grid">
             <div class="grid-col grid-col_main" role="main">
-                <div class="box">
-                    <div class="blurb">
-                        <div class="blurb-hd">
-                            <h1>The page you are trying to reach cannot be found</h1>
-                        </div>
-                        <div class="blurb-bd">
-                            <div class="user-content">
-                                <p>If you typed in the URL, double check to make sure the address was input correctly.</p>
-                                <p>It is possible the page you are trying to reach was moved or no longer exists, please try using the search form or <a href="<?php echo home_url(); ?>">browsing the blog</a>.</p>
+                <div class="archive">
+                    <div class="box">
+                        <div class="blurb blurb_centered">
+                            <div class="blurb-hd blurb-hd_push">
+                                <h1 class="hdg hdg_xxl">The page you are trying to reach cannot be found</h1>
+                            </div>
+                            <div class="blurb-ft">
+                                <div class="user-content">
+                                    <p>If you typed in the URL, double check to make sure the address was input correctly. <br/> It is possible the page you are trying to reach was moved or no longer exists, please try using the search form or <a href="<?php echo home_url(); ?>">browsing the blog</a>.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
