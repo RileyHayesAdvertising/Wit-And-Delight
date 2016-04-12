1373d7bdc4ad17fe3419b40f3d0394a3091c1073
Anthony Ticknor <tticknor@rileyhayes.com>
2016-04-12 14:10:49 -0500

    adjusting vertical rhythm on shop page

--- wp-content/themes/wit-and-delight/includes/loop-shop.php
+++ wp-content/themes/wit-and-delight/includes/loop-shop.php
@@ -3,19 +3,24 @@
     <div class="page-hd">
         <h2 class="isVisuallyHidden"><?php the_title(); ?></h2>
     </div>
-    <div class="page-bd">
-        <?php if ($post->post_content !== "") :?>
+    <?php if ($post->post_content !== "") :?>
+    <div class="page-bd page-bd_push">
         <div class="box">
             <div class="user-content">
                 <?php the_content(); ?>
             </div>
         </div>
-        <?php endif; ?>
     </div>
+    <?php endif; ?>
     <?php endwhile; endif; ?>
     <?php if(get_field('shopstyle_list_id')): ?>
     <div class="page-ft">
-        <?php echo get_shopstyle_products(get_field('shopstyle_list_id')); ?>
+        <div class="section">
+            <div class="section-bd">
+                <h2 class="isVisuallyHidden">Shop these picks!</h2>
+                <?php echo get_shopstyle_products(get_field('shopstyle_list_id')); ?>
+            </div>
+        </div>
     </div>
     <?php endif; ?>
 </div> <!-- // END .page -->
