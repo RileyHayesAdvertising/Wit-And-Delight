0fff7878512d2b4acc1a0eb3c83398525afd946c
Anthony Ticknor <tticknor@rileyhayes.com>
2016-04-25 14:14:25 -0500

    handling edge cases for carousels with 1 or 0 slides

--- wp-content/themes/wit-and-delight/home.php
+++ wp-content/themes/wit-and-delight/home.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
 <?php if (is_home()) : ?>
+<?php if (count(get_field('carousel','options')) > 0) : ?>
 <div class="tier tier_grey">
     <div class="wrapper wrapper_noPad">
         <h2 class="isVisuallyHidden">Featured Content</h2>
-        <div class="carousel" id="js-carousel">
+        <div class="carousel" <?php if (count(get_field('carousel','options')) > 1) : ?>id="js-carousel"<?php endif;?>>
             <div class="carousel-stage">
                 <?php while(has_sub_field('carousel','options')): ?>
                 <div class="carousel-stage-slide">
@@ -22,13 +23,14 @@
                 </div>
                 <?php endwhile; ?>
             </div>
-            <div class="carousel-controls">
+            <div class="carousel-controls <?php if (count(get_field('carousel','options')) < 2) : ?>carousel-controls_alwaysHidden<?php endif;?>">
                 <button class="carousel-controls-prev js-prev-slide"><span class="isVisuallyHidden">Previous Slide</span>&larr;</button>
                 <button class="carousel-controls-next js-next-slide"><span class="isVisuallyHidden">Next Slide</span>&rarr;</button>
             </div>
         </div>
     </div>
 </div>
+<?php endif; ?>
 <?php endif; ?>
 
 <div class="tier tier_grey">
