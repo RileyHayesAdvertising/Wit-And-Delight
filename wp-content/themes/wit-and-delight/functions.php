4374e98a8f6cf2269a97de09f0abd8f21c70cc49
Anthony Ticknor <tticknor@rileyhayes.com>
2016-12-02 14:22:40 -0600

    removing unnecssary caption code from functions

--- wp-content/themes/wit-and-delight/functions.php
+++ wp-content/themes/wit-and-delight/functions.php
@@ -208,18 +208,6 @@ function remove_dimensions( $html ) {
    return $html;
 }
 
-/* gets the thumbnail caption */
-function the_post_thumbnail_caption() {
-  global $post;
-
-  $thumbnail_id    = get_post_thumbnail_id($post->ID);
-  $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
-
-  if ($thumbnail_image && isset($thumbnail_image[0])) {
-    echo '<p class="wp-caption-text">'.$thumbnail_image[0]->post_excerpt.'</p>';
-  }
-}
-
 /* ====================================================================================================
    Comments
 ==================================================================================================== */
